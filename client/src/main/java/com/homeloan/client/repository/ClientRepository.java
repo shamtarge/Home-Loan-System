23faea5c21c1ec9341ea52c89beb5699c243344a
Sham Targe <67789796+shamtarge@users.noreply.github.com>
2023-07-06 22:22:10 +0530

    Delete client directory

--- client/src/main/java/com/homeloan/client/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.homeloan.client.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeloan.client.model.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long>{
-
-}
