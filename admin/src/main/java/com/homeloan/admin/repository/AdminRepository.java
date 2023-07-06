61fe7b64df6a92654adbdcf92e900359dd88433a
Sham Targe <67789796+shamtarge@users.noreply.github.com>
2023-07-06 22:21:44 +0530

    Delete admin directory

--- admin/src/main/java/com/homeloan/admin/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.homeloan.admin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeloan.admin.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-}
